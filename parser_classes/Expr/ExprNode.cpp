ff621b09e6ebdd9b83b0fbc32f95bbdad1490fc9
Смутин Даниил <Dsmutin2003@yandex.ru>
2023-11-24 17:41:06 +0300

    Добавлены методы создания узла выражений от значения, опрерации присваивания и приведения типов

--- parser_classes/Expr/ExprNode.cpp
+++ parser_classes/Expr/ExprNode.cpp
@@ -8,7 +8,120 @@ ExprNode* ExprNode::CreateFromId(std::string* id)
     return tmp;
 }
 
-ExprNode *ExprNode::CreateFromFunctionCall(std::string* id, std::vector<ExprNode*>* listParams)
+ExprNode* ExprNode::CreateFromFunctionCall(std::string* id, std::vector<ExprNode*>* listParams)
 {
-    return nullptr;
+    ExprNode* tmp = new ExprNode();
+    tmp->id = id;
+    tmp->listParams = listParams;
+    tmp->exprType = ExprType::ref_op // Не нашел типа для функции, или просто не понял
+    return tmp;
 }
+
+ExprNode* ExprNode::CreateFromIntValue( int value)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->int_val = value;
+    tmp->exprType = ExprType::int_val;
+    return tmp;
+}
+
+ExprNode* ExprNode::CreateFromFloatValue( float value)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->float_val = value;
+    tmp->exprType = ExprType::float_val;
+    return tmp;
+}
+
+ExprNode* ExprNode::CreateFromStringValue(std::string* value)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->string_val = value;
+    tmp->exprType = ExprType::string_val;
+    return tmp;
+}
+
+ExprNode* ExprNode::CreateFromComStringValue(std::string* value)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->com_string_val = value;
+    tmp->exprType = ExprType::string_val;
+    return tmp;
+}
+
+ExprNode* ExprNode::CreateFromGetValueId(GetValueNode* get_value, std::string* id)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->get_value = get_value;
+    tmp->id = id;
+    tmp->exprType = ExprType::variable;
+    return tmp;
+}
+
+ExprNode* ExprNode::CreateFromAssignOp(ExprNode* left, ExprNode* right)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->left = left;
+    tmp->right = right;
+    tmp->exprType = ExprType::assign_op;
+    return tmp;
+}
+
+ExprNode* ExprNode::CreateFromAssignRefOp(ExprNode* left, ExprNode* right)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->left = left;
+    tmp->right = right;
+    tmp->exprType = ExprType::assign_ref_op;
+    return tmp;
+}
+
+ExprNode* ExprNode::CreateFromIntCast(ExprNode* left)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->left = left;
+    tmp->exprType = ExprType::int_cast;
+    return tmp;
+}
+
+ExprNode* ExprNode::CreateFromFloatCast(ExprNode* left)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->left = left;
+    tmp->exprType = ExprType::float_cast;
+    return tmp;
+}
+
+ExprNode* ExprNode::CreateFromStringCast(ExprNode* left)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->left = left;
+    tmp->exprType = ExprType::string_cast;
+    return tmp;
+}
+
+ExprNode* ExprNode::CreateFromArrayCast(ExprNode* left)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->left = left;
+    tmp->exprType = ExprType::array_cast;
+    return tmp;
+}
+
+ExprNode* ExprNode::CreateFromObjectCast(ExprNode* left)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->left = left;
+    tmp->exprType = ExprType::object_cast;
+    return tmp;
+}
+
+ExprNode* ExprNode::CreateFromBoolCast(ExprNode* left)
+{
+    ExprNode* tmp = new ExprNode();
+    tmp->left = left;
+    tmp->exprType = ExprType::bool_cast;
+    return tmp;
+}
+
+
--- parser_classes/Expr/ExprNode.hpp
+++ parser_classes/Expr/ExprNode.hpp
@@ -1,4 +1,5 @@
-#include "ExprType.cpp"
+#include "../Expr/ExprType.cpp"
+#include "../GetValue/GetValueNode.hpp"
 
 class ExprNode
 {
@@ -17,10 +18,23 @@ public:
 
     static ExprNode* CreateFromId(std::string* id);
     static ExprNode* CreateFromFunctionCall(std::string* id, std::vector<ExprNode*>* listParams);
+    //From values
     static ExprNode* CreateFromIntValue(int int_val);
     static ExprNode* CreateFromFloatValue(float float_val);
     static ExprNode* CreateFromStringValue(std::string* string_val);
     static ExprNode* CreateFromComStringValue(std::string* string_com_val);
     static ExprNode* CreateFromGetValueId(GetValueNode* get_value, std::string* id);
+    //Assign
+    static ExprNode* CreateFromAssignOp(ExprNode* left, ExprNode* right);
+    static ExprNode* CreateFromAssignRefOp(ExprNode* left, ExprNode* right);
+    //Casts
+    static ExprNode* CreateFromIntCast(ExprNode* left);
+    static ExprNode* CreateFromFloatCast(ExprNode* left);
+    static ExprNode* CreateFromStringCast(ExprNode* left);
+    static ExprNode* CreateFromArrayCast(ExprNode* left);
+    static ExprNode* CreateFromObjectCast(ExprNode* left);
+    static ExprNode* CreateFromBoolCast(ExprNode* left);
+    //Right arrow
+
     //TODO продолжить список
 };
