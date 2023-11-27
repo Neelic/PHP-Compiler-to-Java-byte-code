0b5f7bdc69b9e32ad020ce2a318400fd22e2e5b4
Смутин Даниил <Dsmutin2003@yandex.ru>
2023-11-27 14:55:12 +0300

    Добавлены зависимости

--- parser_classes/all_include.hpp
+++ parser_classes/all_include.hpp
@@ -3,4 +3,6 @@
 
 #include "GetValue/GetValueNode.hpp"
 #include "Expr/ExprNode.hpp"
+#include "Stmt/StmtNode.hpp"
+#include "GetValueFunc/GetValueFuncNode.hpp"
 #include "SwitchCaseStmt/SwitchStmtNode.hpp"
