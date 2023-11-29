c66070baf716c377e6cbd57084345027060313b6
Смутин Даниил <Dsmutin2003@yandex.ru>
2023-11-29 21:05:03 +0300

    Реализация конструкторов IfStmtNode

--- parser_classes/IfStmt/IfStmtNode.cpp
+++ parser_classes/IfStmt/IfStmtNode.cpp
@@ -1,16 +1,40 @@
-class IfStmtNode
-{
-private:
-    /* data */
-public:
-    IfStmtNode(/* args */);
-    ~IfStmtNode();
-};
+#include "IfStmtNode.hpp"
 
-IfStmtNode::IfStmtNode(/* args */)
+IfStmtNode* IfStmtNode::CreateFromIfStmt(ExprNode* expr, StmtNode* stmt_main)
 {
+    IfStmtNode* tmp = new IfStmtNode();
+    tmp->expr = expr;
+    tmp->stmt_main = stmt_main;
+    tmp->type = IfStmtType::only_if;
+    return tmp;
 }
 
-IfStmtNode::~IfStmtNode()
+IfStmtNode* IfStmtNode::CreateFromIfElseStmt(ExprNode* expr, StmtNode* stmt_main, StmtNode* stmt_else)
 {
+    IfStmtNode* tmp = new IfStmtNode();
+    tmp->expr = expr;
+    tmp->stmt_main = stmt_main;
+    tmp->stmt_else = stmt_else;
+    tmp->type = IfStmtType::if_else;
+    return tmp;
+}
+
+IfStmtNode* IfStmtNode::CreateFromIfEndIfStmt(ExprNode* expr, std::vector<SmtmNode*>* stmtList)
+{
+    IfStmtNode* tmp = new IfStmtNode();
+    tmp->expr = expr;
+    tmp->stmtList = stmtList;
+    tmp->type = IfStmtType::end_if;
+    return tmp;
+}
+
+IfStmtNode* IfStmtNode::CreateFromIfElseEndIfStmt(ExprNode* expr, StmtNode* stmt_main, StmtNode* stmt_else, std::vector<SmtmNode*>* stmtList)
+{
+    IfStmtNode* tmp = new IfStmtNode();
+    tmp->expr = expr;
+    tmp->stmt_main = stmt_main;
+    tmp->stmt_else = stmt_else;
+    tmp->stmtList = stmtList;
+    tmp->type = IfStmtType::if_else_endif;
+    return tmp;
 }
