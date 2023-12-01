ec6f86f1bf027299c186e5090fa7b2ab899eae5a
Смутин Даниил <Dsmutin2003@yandex.ru>
2023-12-01 18:00:57 +0300

    Сборка зависимостей

--- parser_classes/all_include.hpp
+++ parser_classes/all_include.hpp
@@ -1,8 +1,26 @@
 #include <string>
 #include <vector>
 
+#include "HtmlStmt/HtmlStmtNode.cpp"
 #include "GetValue/GetValueNode.hpp"
-#include "Expr/ExprNode.hpp"
-#include "Stmt/StmtNode.hpp"
 #include "GetValueFunc/GetValueFuncNode.hpp"
+#include "GlobalVar/GlobalVarNode.hpp"
+#include "Expr/ExprNode.hpp"
+#include "StaticVar/StaticVarNode.hpp"
+#include "ConstDecl/ConstDeclNode.hpp"
+#include "MatchStmt/MatchStmtNode.hpp"
+#include "MatchStmt/MatchArmNode.hpp"
+
+#include "Stmt/StmtNode.hpp"
+
+#include "SwitchCaseStmt/CaseDefaultStmtNode.hpp"
 #include "SwitchCaseStmt/SwitchStmtNode.hpp"
+#include "TryCatchStmt/TryStmtNode.hpp"
+#include "TryCatchStmt/CatchStmtNode.hpp"
+#include "TopStmt/TopStmtNode.hpp"
+#include "TryCatchStmt/TryCatchStmtNode.hpp"
+#include "WhileStmt/WhileStmtNode.hpp"
+#include "DoWhileStmt/DoWhileStmtNode.hpp"
+#include "ForEachStmt/ForEachStmtNode.hpp"
+#include "ForStmt/ForStmtNode.hpp"
+#include "IfStmt/IfStmtNode.hpp"
