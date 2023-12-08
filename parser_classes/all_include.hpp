56f1ae02914e47ce0441c9208276ef37f1cf731e
Смутин Даниил <Dsmutin2003@yandex.ru>
2023-12-08 14:06:25 +0300

    Добавлен launch_parser.bat

--- parser_classes/all_include.hpp
+++ parser_classes/all_include.hpp
@@ -1,39 +1,39 @@
 #include <string>
 #include <vector>
 
-#include "Vector classes/ClassAccessModList/ClassAccessModList.hpp"
+#include "Vector_classes/ClassAccessModList/ClassAccessModList.hpp"
 #include "ClassAccessMod/ClassAccessModNode.cpp"
 
 #include "HtmlStmt/HtmlStmtNode.cpp"
 
-#include "Vector classes/IdList/IdListNode.hpp"
+#include "Vector_classes/IdList/IdListNode.hpp"
 
 #include "GetValue/GetValueNode.hpp"
-#include "Vector classes/GetValueFuncList/GetValueFuncList.hpp"
+#include "Vector_classes/GetValueFuncList/GetValueFuncList.hpp"
 #include "GetValueFunc/GetValueFuncNode.hpp"
 
-#include "Vector classes/GlobalVarList/GlobalVarList.hpp"
+#include "Vector_classes/GlobalVarList/GlobalVarList.hpp"
 #include "GlobalVar/GlobalVarNode.hpp"
 
 #include "InterfaceExprDef/InterfaceExprDefNode.hpp"
 
 #include "ClassDef/ClassDefNode.hpp"
 
-#include "Vector classes/ExprList.hpp"
+#include "Vector_classes/ExprList.hpp"
 #include "Expr/ExprNode.hpp"
-#include "Vector classes/ExprFuncList/ExprFuncList.hpp"
+#include "Vector_classes/ExprFuncList/ExprFuncList.hpp"
 #include "ExprFunc/ExprFuncNode.hpp"
 
 #include "FunctionDef/FunctionDefNode.hpp"
 
-#include "Vector classes/InterfaceStmtList/InterfaceStmtList.hpp"
+#include "Vector_classes/InterfaceStmtList/InterfaceStmtList.hpp"
 #include "InterfaceStmt/InterfaceStmtNode.hpp"
 #include "InterfaceStmt/InterfaceStmtDeclNode.hpp"
 
-#include "Vector classes/StaticVarList/StaticVarList.hpp"
+#include "Vector_classes/StaticVarList/StaticVarList.hpp"
 #include "StaticVar/StaticVarNode.hpp"
 
-#include "Vector classes/ConstDeclList/ConstDeclList.hpp"
+#include "Vector_classes/ConstDeclList/ConstDeclList.hpp"
 #include "ConstDecl/ConstDeclNode.hpp"
 
 #include "ClassStmt/ClassStmtDeclNode.hpp"
@@ -42,25 +42,25 @@
 #include "TraitStmtDecl/TraitStmtDeclNode.hpp"
 
 #include "MatchStmt/MatchArmNode.hpp"
-#include "Vector classes/MatchArmList/MatchArmList.hpp"
+#include "Vector_classes/MatchArmList/MatchArmList.hpp"
 #include "MatchStmt/MatchStmtNode.hpp"
 
-#include "Vector classes/StmtList/StmtList.hpp"
+#include "Vector_classes/StmtList/StmtList.hpp"
 #include "Stmt/StmtNode.hpp"
 
 #include "FunctionStmtDecl/FunctionStmtDeclNode.hpp"
 
-#include "Vector classes/ClassStmtList/ClassStmtList.hpp"
+#include "Vector_classes/ClassStmtList/ClassStmtList.hpp"
 #include "ClassStmt/ClassStmtNode.hpp"
 
-#include "Vector classes/CaseDefaultStmtList/CaseDefaultStmtList.hpp"
+#include "Vector_classes/CaseDefaultStmtList/CaseDefaultStmtList.hpp"
 #include "SwitchCaseStmt/CaseDefaultStmtNode.hpp"
 
 #include "SwitchCaseStmt/SwitchStmtNode.hpp"
 // #include "SwitchCaseStmt/CaseDefaultType.cpp"
 // #include "TryCatchStmt/TryStmtNode.hpp"      //! Not supported
 // #include "TryCatchStmt/CatchStmtNode.hpp"    //! Not supported
-#include "Vector classes/TopStmtList/TopStmtList.hpp"
+#include "Vector_classes/TopStmtList/TopStmtList.hpp"
 #include "TopStmt/TopStmtNode.hpp"
 // #include "TryCatchStmt/TryCatchStmtNode.hpp" //! Not supported
 
