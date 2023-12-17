2b641f2e5c1bc22ee3d697636e2ce0b5cf8d5940
Смутин Даниил <Dsmutin2003@yandex.ru>
2023-12-17 16:23:35 +0300

    Реализована часть методов для отрисовки дерева

--- parser_classes/all_include.cpp
+++ parser_classes/all_include.cpp
@@ -4,7 +4,24 @@ std::string GRAPH_STR("digraph Tree {\n");
 
 void printTopStmtList(TopStmtList *node, StartNode *parent);
 void printTopStmt(TopStmtNode *node, TopStmtList *parent);
-void printStmtFromTopStmt(StmtNode *node, TopStmtNode *parent);
+void printStmt(StmtNode *node, std::string id_tag);
+void printIfStmt(IfStmtNode *node, std::string id_tag);
+void printSwitchStmt(SwitchStmtNode *node, std::string id_tag);
+void printStmtList(StmtList *list, std::string id_tag);
+void printStaticVarList(StaticVarList *list, std::string id_tag);
+void printGlobalVarList(GlobalVarList *list, std::string id_tag);
+void printWhileStmt(WhileStmtNode *node, std::string id_tag);
+void printDoWhileStmt(DoWhileStmtNode *node, std::string id_tag);
+void printForStmt(ForStmtNode *node, std::string id_tag);
+void printForEachStmt(ForEachStmtNode *node, std::string id_tag);
+void printMatchStmt(MatchStmtNode *node, std::string id_tag);
+void printConstDeclList(ConstDeclList *list, std::string id_tag);
+void printHtmlStmt(HtmlStmtNode *node, std::string id_tag);
+void printExpr(ExprNode *node, std::string id_tag);
+void printFunctionStmtDecl(FunctionStmtDeclNode *node, std::string id_tag);
+void printClassStmtDecl(ClassStmtDeclNode *node, std::string id_tag);
+void printInterfaceStmtDecl(InterfaceStmtDeclNode *node, std::string id_tag);
+void printTraitStmtDeclNode(TraitStmtDeclNode *node, std::string id_tag);
 
 void printTreeGraph(StartNode *node)
 {
@@ -31,11 +48,72 @@ void printTopStmt(TopStmtNode *node, TopStmtList *parent)
   GRAPH_STR += node->IdTag() + " -> " + parent->IdTag() + "\n";
 
   if (node->stmt != nullptr)
-    printStmtFromTopStmt(node->stmt, node);
+    printStmt(node->stmt, node->IdTag());
+  if (node->func_stmt_decl != nullptr)
+    printFunctionStmtDecl(node->func_stmt_decl, node->IdTag());
+  if (node->class_stmt_decl != nullptr)
+    printClassStmtDecl(node->class_stmt_decl, node->IdTag());
+  if (node->interface_stmt_decl != nullptr)
+    printInterfaceStmtDecl(node->interface_stmt_decl, node->IdTag());
+  if (node->trait_stmt_decl != nullptr)
+    printTraitStmtDeclNode(node->trait_stmt_decl, node->IdTag());
 }
 
-void printStmtFromTopStmt(StmtNode *node, TopStmtNode *parent)
+void printStmt(StmtNode *node, std::string id_tag)
 {
   GRAPH_STR += node->IdTag() + " [label=\"stmt\"];\n";
-  GRAPH_STR += node->IdTag() + " -> " + parent->IdTag() + "\n";
-}
+  GRAPH_STR += node->IdTag() + " -> " + id_tag + "\n";
+
+  if (node->if_stmt != nullptr)
+    printIfStmt(node->if_stmt, node->IdTag());
+  if (node->switch_stmt != nullptr)
+    printSwitchStmt(node->switch_stmt, node->IdTag());
+  if(node->stmtList != NULL)
+    printStmtList(node->stmtList, node->IdTag());
+  if(node->expr_left != nullptr)
+    printExpr(node->expr_left, node->IdTag());
+  if(node->expr_right != nullptr)
+    printExpr(node->expr_right, node->IdTag());
+  if(node->static_var != NULL)
+    printStaticVarList(node->static_var, node->IdTag());
+  if(node->global_var != NULL)
+    printGlobalVarList(node->global_var, node->IdTag());
+  if(node->while_stmt != nullptr)
+    printWhileStmt(node->while_stmt, node->IdTag());
+  if(node->do_while_stmt != nullptr)
+    printDoWhileStmt(node->do_while_stmt, node->IdTag());
+  if(node->for_stmt != nullptr)
+    printForStmt(node->for_stmt, node->IdTag());
+  if(node->foreach_stmt != nullptr)
+    printForEachStmt(node->foreach_stmt, node->IdTag());
+  if(node->match_stmt != nullptr)
+    printMatchStmt(node->match_stmt, node->IdTag());
+  if(node->const_decl != NULL)
+    printConstDeclList(node->const_decl, node->IdTag());
+  if (node->html_stmt != nullptr)
+    printHtmlStmt(node->html_stmt, node->IdTag());
+};
+
+void printIfStmt(IfStmtNode *node, std::string id_tag)
+{
+  GRAPH_STR += node->IdTag() + " [label=\"stmt\"];\n";
+  GRAPH_STR += node->IdTag() + " -> " + id_tag + "\n";
+  
+  if (node->expr != nullptr)
+    printExpr(node->expr, node->IdTag());
+  if (node->stmt_main != nullptr)
+    printStmt(node->stmt_main, node->IdTag());
+  if (node->stmt_else != nullptr)
+    printStmt(node->stmt_else, node->IdTag());
+  if (node->stmtListMain != NULL)
+    printStmtList(node->stmtListMain, node->IdTag());
+  if (node->stmtListElse != NULL)
+    printStmtList(node->stmtListElse, node->IdTag());
+};
+
+void printSwitchStmt(SwitchStmtNode *node, std::string id_tag)
+{
+  GRAPH_STR += node->IdTag() + " [label=\"stmt\"];\n";
+  GRAPH_STR += node->IdTag() + " -> " + id_tag + "\n";
+};
+
