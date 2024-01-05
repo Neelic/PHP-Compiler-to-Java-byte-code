7e7f9aead156c44311c4dd1d3aae91278056a3b2
Смутин Даниил <Dsmutin2003@yandex.ru>
2024-01-05 16:10:32 +0300

    Добавлен ClassScopeContainer

---
+++ semantic/classScopeContainer/ClassScopeContainer.hpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include "../parser_classes/all_include.hpp"
+
+using namespace std;
+
+class ClassStmtDeclNode;
+
+class ClassScopeContainer {
+public:
+
+  ClassStmtDeclNode *node;
+  vector<ExprNode *> variables;
+  vector<ConstDeclNode *> consts;
+  vector<FunctionStmtDeclNode *> functions;
+
+  static ClassScopeContainer *CreateContainer(ClassStmtDeclNode *node, vector<ExprNode *> variables,  vector<ConstDeclNode *> consts, vector<FunctionStmtDeclNode *> functions);
+};
