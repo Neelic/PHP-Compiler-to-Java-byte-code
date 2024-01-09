0b78e5927893ea2254f606f8c41afa40d561fe81
Neelic <salnikov.danila@mail.ru>
2024-01-09 12:57:01 +0300

    Реализация RTL функции echo

--- RTL/Functions.java
+++ RTL/Functions.java
@@ -1,2 +1,11 @@
 class Functions {
+    public void echo(Value value) {
+        if (value == null) return;
+        System.out.println(value.toStringVal().getString());
+    }
+
+    public void echo(String value) {
+        if (value == null) return;
+        System.out.println(value);
+    }
 }
