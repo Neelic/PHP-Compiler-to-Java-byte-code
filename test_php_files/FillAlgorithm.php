ff3d2d8c6926eb5afbf90bc21e8e8414014803b2
Смутин Даниил <Dsmutin2003@yandex.ru>
2024-01-31 06:38:00 +0300

    Еще поправки заливки

--- test_php_files/FillAlgorithm.php
+++ test_php_files/FillAlgorithm.php
@@ -2,115 +2,135 @@
 
 //Заполняем нулями
 
-$arr_matr[0][0] = 0;
-$arr_matr[0][1] = 0;
-$arr_matr[0][2] = 0;
-$arr_matr[0][3] = 0;
-$arr_matr[0][4] = 0;
-$arr_matr[0][5] = 1; //
-$arr_matr[0][6] = 0;
-$arr_matr[0][7] = 0;
-$arr_matr[0][8] = 0;
-$arr_matr[0][9] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 1; //
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
 
-$arr_matr[1][0] = 0;
-$arr_matr[1][1] = 0;
-$arr_matr[1][2] = 0;
-$arr_matr[1][3] = 0;
-$arr_matr[1][4] = 0;
-$arr_matr[1][5] = 1; //
-$arr_matr[1][6] = 0;
-$arr_matr[1][7] = 0;
-$arr_matr[1][8] = 0;
-$arr_matr[1][9] = 0;
+$arr_matr[] = $arr_vect;
 
-$arr_matr[2][0] = 0;
-$arr_matr[2][1] = 0;
-$arr_matr[2][2] = 0;
-$arr_matr[2][3] = 0;
-$arr_matr[2][4] = 1; //
-$arr_matr[2][5] = 0;
-$arr_matr[2][6] = 0;
-$arr_matr[2][7] = 0;
-$arr_matr[2][8] = 0;
-$arr_matr[2][9] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 1; //
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
 
-$arr_matr[3][0] = 0;
-$arr_matr[3][1] = 0;
-$arr_matr[3][2] = 0;
-$arr_matr[3][3] = 1; //
-$arr_matr[3][4] = 0;
-$arr_matr[3][5] = 0;
-$arr_matr[3][6] = 0;
-$arr_matr[3][7] = 0;
-$arr_matr[3][8] = 0;
-$arr_matr[3][9] = 0;
+$arr_matr[] = $arr_vect;
 
-$arr_matr[4][0] = 0;
-$arr_matr[4][1] = 0;
-$arr_matr[4][2] = 0;
-$arr_matr[4][3] = 1; //
-$arr_matr[4][4] = 0;
-$arr_matr[4][5] = 0;
-$arr_matr[4][6] = 0;
-$arr_matr[4][7] = 0;
-$arr_matr[4][8] = 0;
-$arr_matr[4][9] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 1; //
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
 
-$arr_matr[5][0] = 0;
-$arr_matr[5][1] = 0;
-$arr_matr[5][2] = 0;
-$arr_matr[5][3] = 0;
-$arr_matr[5][4] = 1; //
-$arr_matr[5][5] = 0;
-$arr_matr[5][6] = 0;
-$arr_matr[5][7] = 0;
-$arr_matr[5][8] = 0;
-$arr_matr[5][9] = 0;
+$arr_matr[] = $arr_vect;
 
-$arr_matr[6][0] = 0;
-$arr_matr[6][1] = 0;
-$arr_matr[6][2] = 0;
-$arr_matr[6][3] = 0;
-$arr_matr[6][4] = 0;
-$arr_matr[6][5] = 1; //
-$arr_matr[6][6] = 0;
-$arr_matr[6][7] = 0;
-$arr_matr[6][8] = 0;
-$arr_matr[6][9] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 1; //
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
 
-$arr_matr[7][0] = 0;
-$arr_matr[7][1] = 0;
-$arr_matr[7][2] = 0;
-$arr_matr[7][3] = 0;
-$arr_matr[7][4] = 0;
-$arr_matr[7][5] = 0;
-$arr_matr[7][6] = 1; //
-$arr_matr[7][7] = 0;
-$arr_matr[7][8] = 0;
-$arr_matr[7][9] = 0;
+$arr_matr[] = $arr_vect;
 
-$arr_matr[8][0] = 0;
-$arr_matr[8][1] = 0;
-$arr_matr[8][2] = 0;
-$arr_matr[8][3] = 0;
-$arr_matr[8][4] = 0;
-$arr_matr[8][5] = 0;
-$arr_matr[8][6] = 0;
-$arr_matr[8][7] = 1; //
-$arr_matr[8][8] = 0;
-$arr_matr[8][9] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 1; //
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
 
-$arr_matr[9][0] = 0;
-$arr_matr[9][1] = 0;
-$arr_matr[9][2] = 0;
-$arr_matr[9][3] = 0;
-$arr_matr[9][4] = 0;
-$arr_matr[9][5] = 0;
-$arr_matr[9][6] = 0;
-$arr_matr[9][7] = 0;
-$arr_matr[9][8] = 1; //
-$arr_matr[9][9] = 0;
+$arr_matr[] = $arr_vect;
+
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 1; //
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+
+$arr_matr[] = $arr_vect;
+
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 1; //
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+
+$arr_matr[] = $arr_vect;
+
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 1; //
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+
+$arr_matr[] = $arr_vect;
+
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 1; //
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+
+$arr_matr[] = $arr_vect;
+
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 0;
+$arr_vect[] = 1; //
+$arr_vect[] = 0;
+
+$arr_matr[] = $arr_vect;
 
 /*
 0 0 0 0 0 1 0 0 0 0
